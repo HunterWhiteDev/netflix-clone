a9cd0f2f99d9a39a78400364a6d0c8a04ee0e7e4
Hunter <hunterwhitedev@gmail.com>
2021-03-19 09:45:45 -0400

    Init Commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Nav from "./Nav";
+
+function HomeScreen() {
+  return (
+    <div className="HomeScreen">
+      {/* Nav */}
+      <Nav />
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      <h1>TEST</h1>
+      {/* Banner */}
+      {/* Row */}
+      {/* Row */}
+      {/* Row */}
+    </div>
+  );
+}
+
+export default HomeScreen;
