02c17722a35e4e57c093a0b9de756505e14d9dbb
Hunter <hunterwhitedev@gmail.com>
2021-03-19 10:10:15 -0400

    Init Banner Commit

---
+++ src/Banner.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Banner.css";
+function Banner() {
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg')`,
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">This is a test description</h1>
+      </div>
+
+      <div className="banner--fadeButton" />
+    </header>
+  );
+}
+
+export default Banner;
