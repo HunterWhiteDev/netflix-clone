ed97c792f24beca5611957901e7e7db9f2cb40db
Hunter <hunterwhitedev@gmail.com>
2021-03-19 11:24:39 -0400

    Initial Row Commit

--- src/Banner.js
+++ src/Banner.js
@@ -34,16 +34,15 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            "This is a test desctest desctest desctest desctest desctest desctest desctest desc",
-            150
-          )}{" "}
+          {truncate(movie?.overview, 150)}{" "}
         </h1>
       </div>
 
