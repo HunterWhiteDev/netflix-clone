837d7c6a56d9c5f1ae460a11ff1ccd556b6573a3
Hunter <hunterwhitedev@gmail.com>
2021-03-19 10:57:18 -0400

    Setup Axios and random banner feth from tmdb

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log("Movie >>>>> ", movie);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -11,7 +30,7 @@ function Banner() {
       style={{
         backgroundPosition: "center center",
         backgroundSize: "cover",
-        backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
       }}
     >
       <div className="banner__contents">
